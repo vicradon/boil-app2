fc3df4df913f56b0f2049615a547418e7cd3cbcf
Osinachi Chukwujama <osinachi.chukwujama@gmail.com>
2020-11-25 11:57:41 +0100

    fix: images not loading from public folder

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,72 +1,64 @@
-import React, { Component } from 'react';
-import { Media } from 'reactstrap';
-import { Card, CardImg, CardImgOverlay, CardText, CardBody,
-  CardTitle } from 'reactstrap';
+import React, { Component } from "react";
+import { Media } from "reactstrap";
+import {
+  Card,
+  CardImg,
+  CardImgOverlay,
+  CardText,
+  CardBody,
+  CardTitle,
+} from "reactstrap";
 
 class Menu extends Component {
-
   constructor(props) {
-      super(props);
+    super(props);
 
-      this.state = {
-          selectedDish: null
-      }
+    this.state = {
+      selectedDish: null,
+    };
   }
 
   onDishSelect(dish) {
-      this.setState({ selectedDish: dish});
+    this.setState({ selectedDish: dish });
   }
 
   renderDish(dish) {
-      if (dish != null)
-          return(
-              <Card onClick = {() => this.onDishSelect(dish)}>
-                  <CardImg top src={dish.image} alt={dish.name} />
-                  <CardBody>
-                    <CardTitle>{dish.name}</CardTitle>
-                    <CardText>{dish.description}</CardText>
-                  </CardBody>
-              </Card>
-          );
-      else
-          return(
-              <div></div>
-          );
+    if (dish != null)
+      return (
+        <Card onClick={() => this.onDishSelect(dish)}>
+          <CardImg top src={dish.image} alt={dish.name} />
+          <CardBody>
+            <CardTitle>{dish.name}</CardTitle>
+            <CardText>{dish.description}</CardText>
+          </CardBody>
+        </Card>
+      );
+    else return <div></div>;
   }
 
+  render() {
+    const menu = this.props.dishes.map((dish) => {
+      console.log(dish.image)
+      return (
+        <div key={dish.id} className="col-12 cil-md-5 m-1">
+          <Card>
+            <CardImg width="100%" src={`${process.env.PUBLIC_URL}/${dish.image}`} alt={dish.name} />
 
-    render() {
-        const menu = this.props.dishes.map((dish) => {
-            return (
-              <div key={dish.id} className="col-12 cil-md-5 m-1">
-                <Card>
-                
-                  
-                    <CardImg width = "100%" src={dish.image} alt={dish.name} />
-                  
-                  <CardImgOverlay>
-                    <CardTitle>{dish.name}</CardTitle>
-                    
-                 
-                  </CardImgOverlay>
-                
-                </Card> 
-              </div>
-            );
-        });
+            <CardImgOverlay>
+              <CardTitle>{dish.name}</CardTitle>
+            </CardImgOverlay>
+          </Card>
+        </div>
+      );
+    });
 
-        return (
-          <div className="container">
-            <div className="row">
-             
-                  {menu}
-            </div>
-            <div className="row">
-              {this.renderDish(this.state.selectedDish)}
-            </div>
-          </div>
-        );
-    }
+    return (
+      <div className="container">
+        <div className="row">{menu}</div>
+        <div className="row">{this.renderDish(this.state.selectedDish)}</div>
+      </div>
+    );
+  }
 }
 
 export default Menu;
